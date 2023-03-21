7c9162b50195f328b0e51e09d3f1f96b99f2e6da
maryhuziuk <116351558+maryhuziuk@users.noreply.github.com>
2023-03-21 11:04:08 +0200

    changes

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,8 +2,9 @@ const items = categories.querySelectorAll(".item");
 console.log(`Number of categories: ${items.length}`);
 
 items.forEach((item) => {
-  const elements = item.querySelectorAll("li");
   console.log(``);
   console.log(`Category: ${item.firstElementChild.textContent}`);
-  console.log(`Elements: ${elements.lastElementChild.length}`);
+  console.log(
+    `Elements: ${item.lastElementChild.querySelectorAll("li").length}`
+  );
 });
